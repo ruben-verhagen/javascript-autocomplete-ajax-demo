66e7c70f50b636fbc96a05521b19645b49c43671
Ruben Verhagen <ruben.verhagen101@gmail.com>
2016-01-31 11:30:58 +0100

    added type field to the front-end, more sample data added

--- ajax/feed.php
+++ ajax/feed.php
@@ -47,7 +47,62 @@ $dataset =  array(
     "id" => "49273629993",
     "name" => "500 Brickell (condo)",
     "type" => "condo"
-  )
+  ),
+  array(
+    "id" => "8638640563143",
+    "name" => "Carbonell Brickell Key (condo in Brickell)",
+    "type" => "condo"
+  ),
+  array(
+    "id" => "7842279356592",
+    "name" => "BRI37849592 (condo in Brickell)",
+    "type" => "listing"
+  ),
+  array(
+    "id" => "45747373133",
+    "name" => "Courts Brickell Key (condo in Brickell Key)",
+    "type" => "condo"
+  ),
+  array(
+    "id" => "743639398734",
+    "name" => "The Plaza On Brickell (complex in Brickell)",
+    "type" => "complex"
+  ),
+  array(
+    "id" => "29383839211",
+    "name" => "Il Villaggio (condo in South Beach)",
+    "type" => "condo"
+  ),
+  array(
+    "id" => "3248101227",
+    "name" => "Il Pellicano (condo in Coconut Grove)",
+    "type" => "condo"
+  ),
+  array(
+    "id" => "7923233390179",
+    "name" => "A328364900 (house in Coral Gables",
+    "type" => "listing"
+  ),
+  array(
+    "id" => "4732900836",
+    "name" => "A3283679 (office space in Wynwood)",
+    "type" => "listing"
+  ),
+  array(
+    "id" => "595937736639923253",
+    "name" => "Village Gables (in Gables On The Bay)",
+    "type" => "complex"
+  ),
+  array(
+    "id" => "33297460",
+    "name" => "Icon West (in Doral)",
+    "type" => "condo"
+  ),
+  array(
+    "id" => "739347363876232093",
+    "name" => "A74836400434455436 (house in Pinecrest Hollow)",
+    "type" => "listing"
+  ),  
 );
 
 $results = [];
